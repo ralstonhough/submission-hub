036816da0faa6bf30d6225cb8c524b77cafad330
Ralston Hough <ralstonhough@gmail.com>
2022-09-26 14:20:11 -0400

    Adding links etc.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let circularcursor = document.getElementById("circularcursor");
+let links = document.getElementsByTagName("a");
 
 document.onmousemove = function(){
   circularcursor.style.top = event.clientY + "px";
