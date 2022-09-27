13d9520d857e96f9f564626476de319af980dedd
Ralston Hough <ralstonhough@gmail.com>
2022-09-26 20:38:35 -0400

    Added some effects✨

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let circularcursor = document.getElementById("circularcursor");
 let links = document.getElementsByTagName("a");
+let canvas = document.getElementById("canvas");
 
-document.onmousemove = function(){
-  circularcursor.style.top = event.clientY + "px";
-  circularcursor.style.left = event.clientX + "px";
+document.onmousemove = function(e){
+  circularcursor.style.top = e.clientY + "px";
+  circularcursor.style.left = e.clientX + "px";
+  canvas.style.background = "hsl(0,0%," + map(e.clientY, 0, window.innerHeight, 25, 100) + "%)";
 };
+
+function map(value, low1, high1, low2, high2) {
+  return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
